f02055602975eb01896d91fa1346cabb46f0782b
mnoah <noahvrazo@gmail.com>
2016-09-08 11:51:39 -0400

    working

--- Lab7_NDV/Program.cs
+++ Lab7_NDV/Program.cs
@@ -3,13 +3,84 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GC_CLassroom;
 
 namespace Lab7_NDV
 {
-    class Program
+  public  class Program
     {
-        static void Main(string[] args)
+       public static void Main(string[] args)
         {
+
+            Classroom expNet = new Classroom();
+            Classroom javaClass = new Classroom();
+            string Anw;
+
+
+            Console.WriteLine("Welcome to our .Net class. Which student would you like to klnow about?");
+            Console.WriteLine("Enter a Number between 1 and 20");
+            string student =Console.ReadLine();
+            int studentNum = int.Parse(student);
+
+            while (studentNum > 20) 
+            {
+                Console.WriteLine("Out of Rang");
+
+                Console.WriteLine("Enter a Number between 1 and 20");
+                 student = Console.ReadLine();
+                 studentNum = int.Parse(student);
+
+
+            } 
+
+
+                Console.WriteLine("Student " + studentNum + " is " + expNet.m_Student[studentNum].Name);
+
+            
+            
+              
+                    Console.WriteLine("What would you like to know about " + expNet.m_Student[studentNum].Name + "? \nEnter: 1 for 'homeTown', 2 for 'favorite food' 3 for 'age'");
+
+                string info = Console.ReadLine();
+                int infoNum = int.Parse(info);
+            while (!(info == "1" || info == "2" || info == "3"))
+            {
+                Console.WriteLine("Non data. Please enter 1, 2 or 3");
+
+                info = Console.ReadLine();
+                infoNum = int.Parse(info);
+            }
+
+                do
+                { 
+
+
+
+                    if (info == "1")
+                    {
+                        Console.WriteLine(expNet.m_Student[studentNum].HomeTown);
+                    }
+                    else if (info == "2")
+                    {
+                        Console.WriteLine(expNet.m_Student[studentNum].FavFood);
+                    }
+                    else if (info == "3")
+                    {
+                        Console.WriteLine(expNet.m_Student[studentNum].Age);
+
+
+                    }
+                    Console.WriteLine("would you like to know more? (yes/no)");
+                    Anw = Console.ReadLine().ToLower();
+                } while (Anw == "yes");
+
+                Console.WriteLine("Thanks!");
+            
+
+            
+
+
+
         }
     }
 }
---
+++ Students/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Students
+{
+    public class Classroom
+    {
+        
+            private Student[] m_Student = new Student[20];
+            public Classroom()
+            {
+                for (int i = 0; i < m_Student.Length; i++)
+                {
+                    m_Student[i] = new Student();
+                }
+                m_Student[19].Name = "Kim";
+                m_Student[19].HomeTown = "Detroit";
+                m_Student[19].FavFood = "Cake";
+                m_Student[19].Gender = "F";
+
+            }
+        }
+}
